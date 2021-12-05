bba9d6296dcd8fbea55f313266bc15acccc81211
Nisan Ayash <NisanA@radware.com>
2021-12-05 09:29:28 +0200

    data structre directory created

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+// Data Structure
+
+// Algorithms
+
+// Design Patterns
+
 import Structural from "./src/design-patterns/structural/index";
 import Creational from "./src/design-patterns/creational/index";
 import Behavioral from "./src/design-patterns/behavioral/index";
